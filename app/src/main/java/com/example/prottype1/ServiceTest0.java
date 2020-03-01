ec6e4663e1bf06c6dc4e95be054a11bd5d10427f
econron <50755382+econron@users.noreply.github.com>
2020-03-01 13:11:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/prottype1/ServiceTest0.java
@@ -0,0 +1,14 @@
+package com.example.prottype1;
+
+import android.app.Activity;
+
+public class ServiceTest0 extends Activity {
+    Button startButton;
+    Button stopButton;
+
+    @Override
+    public void onCreate(IBundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
